648f40e5f1e9ad09cc4df261bf52822f5fc6e9ea
Tarlan Jabiev <48480900+TarlanJabiyev@users.noreply.github.com>
2023-01-04 14:10:43 +0200

    Add files via upload

---
+++ OpenAI Shiny.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(openai)
+library(shiny)
+library(shinythemes)
+library(shinymanager)
+
+ui <- fluidPage(
+  navbarPage(
+    title = "OpenAI Shiny", 
+    theme = shinytheme(theme = "readable"), 
+    
+    fluidRow(
+      column(
+        width = 1
+      ),
+      column(
+        width = 10,
+        textAreaInput("ask_something", "Ask something", "", width = "1100px"),
+        actionButton("action", "Response", icon = icon("fa-solid fa-pen-nib", style = "color: #1E90FF")),
+        br(),hr()
+      ),
+      column(
+        width = 1
+      )
+    ),
+    fluidRow(
+      column(
+        width = 1
+      ),
+      column(
+        width = 8,
+        textOutput("response1")
+      ),
+      column(
+        width = 1
+      )
+    )
+  )
+)
+
+# credentials ----
+credentials <- data.frame(
+  user = c("Harmony"),
+  password = c("Super1@User2"), 
+  stringsAsFactors = F
+)
+ui <- secure_app(ui)
+
+server <- function(input, output, session) {
+  
+  res_auth <- secure_server(
+    check_credentials = check_credentials(credentials)
+  )
+  reactive_credentials <- reactive({reactiveValuesToList(res_auth)})
+  
+  # Page 1 ----
+  response <- reactive({
+    api_key <- "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"
+    
+    text_prompt <- input$ask_something
+    
+    chatGHP <- create_completion(
+      model = "text-davinci-003",
+      prompt = text_prompt,
+      max_tokens = 1000,
+      openai_api_key = api_key,
+      temperature = 0
+    )
+    
+    chatGHP$choices$text
+  })
+  
+  output$response1 <- renderText({
+    input$action
+    isolate(response())
+  })
+}
+
+shinyApp(ui, server)
